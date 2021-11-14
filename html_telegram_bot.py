2a38c4c3a0f9062732fb97b336495701f2d093af
Liuyi Sun <sly.thu@gmail.com>
2021-11-13 16:22:16 -0800

    better looking log dumper

--- html_telegram_bot.py
+++ html_telegram_bot.py
@@ -10,6 +10,7 @@ class HTMLTelegramBot:
         self.html_file = open('./replay/index.html', 'w')
         self.write_header()
         self.image_count = 0
+        self.event_sequence = 0
 
     def write_header(self):
         self.html_file.write('''<!DOCTYPE html>
@@ -23,25 +24,31 @@ class HTMLTelegramBot:
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
   <script>
   </script>
-  <table>
+  <style>
+  td{
+  border:1px solid
+  }
+  </style>
+  <table id="table">
     <tbody>
-        
+        <thead>
+            <th>Event Sequence</th>
+            <th>Type</th>
+            <th>Details</th>
+            <th>DateTime</th>                        
+        </thead>
 ''')
 
     def write_footer(self):
-        self.html_file.write('''
-        </tbody>
-        </table>
-</body>
-</html>
-''')
+        self.html_file.write('''</tbody></table></body></html>''')
         url = 'file://' + str(Path(self.html_file.name).absolute())
         self.html_file.flush()
         self.html_file.close()
         webbrowser.open(url, new=2)
 
     def send_text_message(self, message):
-        self.html_file.write('<tr><td>%s</td></tr>\n' % message)
+        self.html_file.write(f'<tr><td>{self.event_sequence}</td><td>Text Message</td><td>{message}</td></tr>\n')
+        self.event_sequence += 1
 
     def edit_image_message(self, chat_id: str, message_id: str, base64_encoded_image, filename: str = ''):
         f = open('replay/images/image_%d.jpg' % self.image_count, 'wb')
@@ -49,25 +56,29 @@ class HTMLTelegramBot:
         f.write(file_content)
         f.close()
         self.html_file.write(
-            '''<tr><td>
-            A previously posted image [%s] was updated, new image is:
+            f'''<tr><td>{self.event_sequence}</td><td>Edit Image</td>
+            <td>
+            A previously posted image [{message_id}] was updated, new image is:
             <br>
-            <a href="images/image_%d.jpg">
-            <img style="width:300px;height:200px" src="images/image_%d.jpg" /></a></td></tr>\n''' % (
-                message_id, self.image_count, self.image_count))
-        self.image_count = self.image_count + 1
+            <a href="images/image_{self.image_count}.jpg">
+            <img style="width:300px;height:200px" src="images/image_{self.image_count}.jpg" /></a></td></tr>\n''')
+        self.event_sequence += 1
+        self.image_count += 1
 
     def pin_message(self, chat_id: str, message_id: str) -> bool:
         message = 'Pinning messages for room [%s], message id: [%s]' % (chat_id, message_id)
-        self.html_file.write('<tr><td>%s</td></tr>\n' % message)
+        self.html_file.write(f'<tr><td>{self.event_sequence}</td><td>Pin Message</td><td>{message}</td></tr>\n')
+        self.event_sequence += 1
 
     def unpin_message(self, chat_id: str, message_id: str) -> bool:
         message = 'Unpinning messages for room [%s], message id: [%s]' % (chat_id, message_id)
-        self.html_file.write('<tr><td>%s</td></tr>\n' % message)
+        self.html_file.write(f'<tr><td>{self.event_sequence}</td><td>Unpin Message</td><td>%s</td></tr>\n')
+        self.event_sequence += 1
 
     def unpin_all_messages(self, chat_id: str) -> bool:
         message = 'Unpinning all messages for room [%s]' % chat_id
-        self.html_file.write('<tr><td>%s</td></tr>\n' % message)
+        self.html_file.write(f'<tr><td>{self.event_sequence}</td><td>Unpin all Messages</td><td>%s</td></tr>\n')
+        self.event_sequence += 1
 
     def send_image_message(self, base64_encoded_image, filename: str = '', caption: str = '',
                            as_document: bool = True):
@@ -76,8 +87,9 @@ class HTMLTelegramBot:
         f.write(file_content)
         f.close()
         self.html_file.write(
-            '''<tr><td><a href="images/image_%d.jpg">
-            <img style="width:300px;height:200px" src="images/image_%d.jpg" /></a></td></tr>\n''' % (
-                self.image_count, self.image_count))
-        self.image_count = self.image_count + 1
-        return 'dummy_chat_id','dummy_message_id'
+            f'''<tr><td>{self.event_sequence}</td><td>Send Image</td>
+            <td><a href="images/image_{self.image_count}.jpg">
+            <img style="width:300px;height:200px" src="images/image_{self.image_count}.jpg" /></a></td></tr>\n''')
+        self.image_count += 1
+        self.event_sequence += 1
+        return 'dummy_chat_id', 'dummy_message_id'
