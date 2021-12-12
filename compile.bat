5634cb7016b596ca4a7fb5b2e8641e5992e6f072
Liuyi Sun <sly.thu@gmail.com>
2021-12-12 00:07:11 -0800

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -2,12 +2,13 @@
 
 RMDIR /S /Q dist
 RMDIR /S /Q build
+RMDIR /S /Q __pycache__
 
 WHERE pyinstaller
 IF %ERRORLEVEL% NEQ 0 (
     ECHO "pyinstaller wasn't found, try to install it via pip3 now"
     pip3 install pyinstaller
-) ELSE (
-    pyinstaller -F bot.spec
 )
+
+pyinstaller -F bot.spec
 
